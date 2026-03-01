db78431fef917eb53004088a6ca3a5a8e9d666fb
Vadim Vinogradov <vadim.vinogradov@gmail.com>
2026-03-02 02:47:30 +0400

    Cleanup eslint config

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import prettier from 'eslint-config-prettier';
+
+export default [
+  js.configs.recommended,
+  { languageOptions: { globals: globals.node } },
+  prettier,
+];
