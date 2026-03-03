eb443b1d729517e8aae327d5492790e9955cd71b
Vadim Vinogradov <vadim.vinogradov@gmail.com>
2026-03-03 10:06:50 +0400

    Update README and tests to enhance clarity on filename matching rules

--- is-valid.test.js
+++ is-valid.test.js
@@ -4,8 +4,10 @@ import { isValid } from './is-valid.js';
 
 it('isValid', () => {
   assert.strictEqual(isValid('index.ts', 'MyExport'), true);
-  assert.strictEqual(isValid('my-file.ts', 'MyFile'), true);
-  assert.strictEqual(isValid('my-file.ext.ts', 'MyFileExt'), true);
+  assert.strictEqual(isValid('my-function.ts', 'myFunction'), true);
+  assert.strictEqual(isValid('my-super-function.ts', 'mySuperFunction'), true);
+  assert.strictEqual(isValid('my-class.ts', 'MyClass'), true);
+  assert.strictEqual(isValid('my-class.service.ts', 'MyClassService'), true);
   assert.strictEqual(isValid('my-constant.ts', 'MY_CONSTANT'), true);
   assert.strictEqual(isValid('my-file.ts', 'MyExport'), false);
   assert.strictEqual(isValid('my-file.ext.ts', 'MyFile'), false);
